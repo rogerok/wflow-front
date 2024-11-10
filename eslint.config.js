8ee01c1001748e00c0dd043f69f3a01cf063eb1f
v.fedorik <fedorik833@gmail.com>
2024-11-10 18:39:38 +0300

    project configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
+const baseConfig = require('./eslint.base.config.js');
 const nx = require('@nx/eslint-plugin');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const tsParser = require('@typescript-eslint/parser');
 
 module.exports = [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
+  ...baseConfig,
   {
     ignores: ['**/dist'],
   },
@@ -17,5 +18,44 @@ module.exports = [
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     // Override or add rules here
     rules: {},
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      '@typescript-eslint/explicit-module-boundary-types': ['error'],
+    },
+  },
+  {
+    files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+            {
+              sourceTag: 'type:ui',
+              onlyDependOnLibsWithTags: ['type:ui'],
+            },
+          ],
+        },
+      ],
+    },
   },
 ];
