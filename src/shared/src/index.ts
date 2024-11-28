294e84cc26b49cfbe0bb1f2bcefd520c01fd0698
v.fedorik <fedorik833@gmail.com>
2024-11-28 20:13:37 +0300

    updated button's styles

--- src/shared/src/index.ts
+++ src/shared/src/index.ts
@@ -1,6 +1,9 @@
 //ui
 export * from './lib/ui/Button/Button';
+export * from './lib/ui/Flex/Flex';
+export * from './lib/ui/HStack/HStack';
 export * from './lib/ui/IconComponent/IconComponent';
+export * from './lib/ui/VStack/VStack';
 // layouts
 export * from './lib/layouts';
 // constants
