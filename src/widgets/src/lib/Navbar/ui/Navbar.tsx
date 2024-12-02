b96b43d46873ec6c3b526129661c13f04cb91c98
vyacheslav <fedorik833@gmail.com>
2024-12-02 17:51:07 +0300

    added text label to nav's links

--- src/widgets/src/lib/Navbar/ui/Navbar.tsx
+++ src/widgets/src/lib/Navbar/ui/Navbar.tsx
@@ -15,36 +15,38 @@ const NavLinks = [
     to: routes.main(),
     inActive: <IconComponent name={'HomeIcon'} size={'sm'} />,
     active: <IconComponent name={'HomeIconFilled'} size={'sm'} />,
+    label: 'Главная',
   }),
   linkOptions({
     to: '/',
     inActive: <IconComponent name={'TimerIcon'} size={'sm'} />,
     active: <IconComponent name={'TimerIconFilled'} size={'sm'} />,
+    label: 'Что-нибудь',
   }),
   linkOptions({
     to: routes.statistic(),
     inActive: <IconComponent name={'StatisticIcon'} size={'sm'} />,
     active: <IconComponent name={'StatisticIconFilled'} size={'sm'} />,
+    label: 'Статистика',
   }),
   linkOptions({
     to: routes.settings(),
     inActive: <IconComponent name={'SettingsIcon'} size={'sm'} />,
     active: <IconComponent name={'SettingsIconFilled'} size={'sm'} />,
+    label: 'Настройки',
   }),
 ];
 
 export const Navbar: FC<NavbarProps> = (props) => {
   return (
-    <nav
-      className={cnNavbar(undefined, [props.className])}
-      // flexJustify={'between'}
-      // align={'center'}
-      // as={'nav'}
-    >
+    <nav className={cnNavbar(undefined, [props.className])}>
       {NavLinks.map((link) => (
-        <AppLink className={cnNavbar('Link')} to={link.to} key={link.to}>
-          {({ isActive }) => (isActive ? link.active : link.inActive)}
-        </AppLink>
+        <HStack align={'center'} gap={'16'}>
+          <AppLink className={cnNavbar('Link')} to={link.to} key={link.to}>
+            {({ isActive }) => (isActive ? link.active : link.inActive)}
+          </AppLink>
+          <span>{link.label}</span>
+        </HStack>
       ))}
     </nav>
   );
