c544663782306b9cdbb471ad69dd86bf05c00780
vyacheslav <fedorik833@gmail.com>
2025-01-27 16:26:41 +0300

    fixed private routing

--- src/app/routes/_protected.tsx
+++ src/app/routes/_protected.tsx
@@ -1,15 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { routes } from '@shared';
 
 export const Route = createFileRoute('/_protected')({
-  // beforeLoad: ({ context }) => {
-  //   if (!context.isAuth) {
-  //     context.authController?.restoreSession().then(() => {
-  //       if (!context.isAuth) {
-  //         throw redirect({
-  //           to: routes.signIn(),
-  //         });
-  //       }
-  //     });
-  //   }
-  // },
+  beforeLoad: ({ context }) => {
+    if (!context.isAuth) {
+      throw redirect({
+        to: routes.main(),
+      });
+    }
+  },
 });
