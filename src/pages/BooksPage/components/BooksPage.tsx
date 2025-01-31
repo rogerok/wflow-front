4dee0ea73d1577696115613bfc90a6fc23f99699
vyacheslav <fedorik833@gmail.com>
2025-01-31 16:44:25 +0300

    updates styles

--- src/pages/BooksPage/components/BooksPage.tsx
+++ src/pages/BooksPage/components/BooksPage.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@bem-react/classname';
 import { routes } from '@shared/const';
-import { ButtonLink, Page } from '@shared/elements';
+import { ButtonLink, Page, VStack } from '@shared/elements';
 import { BooksService } from '@shared/services';
 import { observer } from 'mobx-react-lite';
 import { FC, useEffect, useState } from 'react';
@@ -24,12 +24,19 @@ export const BooksPage: FC<BooksPageProps> = observer((props) => {
 
   return (
     <Page className={cnBooksPage(undefined, [props.className])}>
-      <ButtonLink to={routes.booksCreate()}>
-        Добавить книгу бант линк
-      </ButtonLink>
-      {service.data.map((book) => (
-        <p key={book.id}>{book.name}</p>
-      ))}
+      <ButtonLink to={routes.booksCreate()}>Добавить</ButtonLink>
+
+      <VStack as={'ul'} gap={'16'} fullWidth>
+        {service.data.map((book) => (
+          <VStack gap={'8'} as={'li'}>
+            <h3 key={book.id}>{book.name}</h3>
+            <p key={book.id}>{book.description}</p>
+            <p key={book.id}>
+              Создано {new Date(book.createdAt).toLocaleDateString()}
+            </p>
+          </VStack>
+        ))}
+      </VStack>
     </Page>
   );
 });
