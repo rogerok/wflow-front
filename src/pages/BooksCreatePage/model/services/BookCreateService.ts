26baf8a6ea9a7ba99f2a782268d040e0a837dfe4
Slavik <55946493+rogerok@users.noreply.github.com>
2025-02-04 12:56:32 +0300

    configured eslint to forbid absolute imports from same folder (#34)

--- src/pages/BooksCreatePage/model/services/BookCreateService.ts
+++ src/pages/BooksCreatePage/model/services/BookCreateService.ts
@@ -1,8 +1,9 @@
-import { createBookRequest } from '@pages/BooksCreatePage/model/api/createBookApi';
 import { convertEmptyStringToNull, FormStore } from '@shared/lib';
 import { RequestStore } from '@shared/stores';
 import { BookFormRequestSchema, BookFormRequestType } from '@shared/types';
 import { makeAutoObservable, runInAction } from 'mobx';
+
+import { createBookRequest } from '../api/createBookApi';
 
 export class BookCreateService {
   form = new FormStore<BookFormRequestType>({
