af5cbe80248d25d89a07db78898edc2ebb9d1b57
Slavik <55946493+rogerok@users.noreply.github.com>
2025-03-02 14:07:51 +0300

    Main (#69)
    * merge development (#27)
    
    * Merge pull request #16
    
    * pages moved to protected routes
    
    * added session restoring
    
    * added role based navbar displaying
    
    * updated config
    
    * reorg imports and exports
    
    * try fix circ deps
    
    * fix some circle deps
    
    * improved imports
    
    * remove dependency
    
    * Merge pull request #17
    
    * pages moved to protected routes
    
    * added session restoring
    
    * added role based navbar displaying
    
    * updated config
    
    * reorg imports and exports
    
    * Merge branch 'development' into base-private-rounting
    
    * remove dependency
    
    * shortened import
    
    * User logout (#18)
    
    * added logout method
    
    * styled logout button
    
    * added redirection after logout
    
    * fixed private routing (#19)
    
    * added new options in navbar (#20)
    
    * Merge pull request #21
    
    * fixed styles in navbar
    
    * fixed styles in navbar (#22)
    
    * added navbar closing for mobile devices on location change (#23)
    
    * added listening to removing token from local storage (#24)
    
    * Global store router (#25)
    
    * added passing router instance to global store
    
    * global store wrapped in useState
    
    * fix linting (#26)
    
    * fix linting
    
    * change ci branch
    
    * fix confclits
    
    * mr to main (#68)
    
    * Registration page (#64)
    
    * created box component, some styles in form
    
    * fixed type of props in VStack HStack
    
    * fix flex direction
    
    * updated theme, added sigun up fields and created paper component
    
    * layout changed
    
    * created grid component (#65)
    
    * changed form submit model (#66)

---
+++ src/app/routes/_protected/books.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { BooksPage } from '@pages';
+
+export const Route = createFileRoute('/_protected/books')({
+  component: BooksPage,
+});
