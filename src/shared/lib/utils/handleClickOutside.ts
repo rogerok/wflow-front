a385f1c5a614a506e602fad64ad38b4b30b49abe
Slavik <55946493+rogerok@users.noreply.github.com>
2025-03-07 14:13:52 +0300

    updated displaying foal info (#81) (#82)

--- src/shared/lib/utils/handleClickOutside.ts
+++ src/shared/lib/utils/handleClickOutside.ts
@@ -9,48 +9,27 @@ export type HandleClickOutsideReturnType = (
   event: MouseEvent | TouchEvent,
 ) => void;
 
-// export const handleClickOutside = ({
-//   ref,
-//   callback,
-// }: handleClickOutsideArgs): HandleClickOutsideReturnType => {
-//   return (event: MouseEvent | TouchEvent): void => {
-//     const target = event.target as Node;
-//
-//     let isOutside = false;
-//
-//     if (Array.isArray(ref)) {
-//       isOutside = ref
-//         .filter((r) => Boolean(r.current))
-//         .every((r) => !r.current?.contains(target));
-//     } else if (ref?.current && !Array.isArray(ref) && 'contains' in ref) {
-//       isOutside = 'contains' in ref.current && ref.current.contains(target);
-//     }
-//
-//     if (isOutside) {
-//       callback(event);
-//     }
-//   };
-// };
-
 export const handleClickOutside = ({
   ref,
   callback,
 }: handleClickOutsideArgs): HandleClickOutsideReturnType => {
   return (event: MouseEvent | TouchEvent): void => {
-    const target = event.target as Node;
+    const target = event.target;
 
     let isOutside = false;
 
-    if (Array.isArray(ref)) {
-      isOutside = ref
-        .filter((r) => Boolean(r.current))
-        .every((r) => r.current && !r.current.contains(target)); // Corrected
-    } else if (ref?.current) {
-      isOutside = !ref.current.contains(target); // Corrected
-    }
+    if (target instanceof Node) {
+      if (Array.isArray(ref)) {
+        isOutside = ref
+          .filter((r) => Boolean(r.current))
+          .every((r) => r.current && !r.current.contains(target));
+      } else if (ref?.current) {
+        isOutside = !ref.current.contains(target);
+      }
 
-    if (isOutside) {
-      callback(event);
+      if (isOutside) {
+        callback(event);
+      }
     }
   };
 };
