7df213997c1e8c42e6b57699dde86beee4c95916
v.fedorik <fedorik833@gmail.com>
2025-04-05 13:02:27 +0300

    created tests for RequestStore

--- src/shared/stores/request/RequestStore.test.ts
+++ src/shared/stores/request/RequestStore.test.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
+import { when } from 'mobx';
+import { toast } from 'react-toastify';
 import { beforeEach, describe } from 'vitest';
 
 import { RequestStore } from './RequestStore';
 
+type MockData = {
+  user: string;
+};
+const mockData: MockData = {
+  user: 'John Doe',
+};
+
 vi.mock('axios');
+const mockedAxios = vi.mocked(axios, true);
 
 vi.mock('react-toastify', () => ({
   toast: {
@@ -12,20 +22,52 @@ vi.mock('react-toastify', () => ({
   },
 }));
 
-type MockData = {
-  user: string;
-};
-
 describe('request store test', () => {
-  let store: RequestStore<MockData, any[]>;
-
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  it('request - success', () => {
-    store = new RequestStore(axios.get, undefined);
+  it('request - success', async () => {
+    mockedAxios.get.mockReturnValue(Promise.resolve({ data: mockData }));
+
+    const store = new RequestStore<MockData>(() => mockedAxios.get(''), {
+      success: 'success',
+    });
 
     expect(store.result).toEqual({ data: null, status: 'idle' });
+
+    store.call();
+
+    expect(store.isLoading).toBe(true);
+    expect(store.result).toEqual({ data: null, status: 'loading' });
+
+    await when(() => store.result.status === 'success');
+    expect(store.result).toEqual({
+      data: mockData,
+      status: 'success',
+    });
+
+    expect(toast.success).toHaveBeenCalledWith('success');
+  });
+
+  it('request - success', async () => {
+    const error = new Error('something went wrong');
+    mockedAxios.get.mockRejectedValue(error);
+
+    const store = new RequestStore<MockData>(() => mockedAxios.get(''));
+
+    expect(store.result).toEqual({ data: null, status: 'idle' });
+
+    store.call();
+
+    expect(store.isLoading).toBe(true);
+    expect(store.result).toEqual({ data: null, status: 'loading' });
+
+    await when(() => store.result.status === 'error');
+    expect(store.result).toEqual({
+      data: null,
+      status: 'error',
+      error,
+    });
   });
 });
