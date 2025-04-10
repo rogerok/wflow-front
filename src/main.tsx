e98d93e0958d0b8c394d700868be1a8a5b51481e
vyacheslav <fedorik833@gmail.com>
2025-04-10 17:05:03 +0300

    husky installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-// eslint-disable-next-line
+/* eslint-disable simple-import-sort/imports */
 import { scan } from 'react-scan'; // import this BEFORE react
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
 import App from './app/app';
+/* eslint-enable simple-import-sort/imports */
 
 if (typeof window !== 'undefined') {
   scan({
